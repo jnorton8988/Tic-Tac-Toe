cdb69555b4e07c25d7b83100f33751da397fb00a
jnorton8988 <93297487+jnorton8988@users.noreply.github.com>
2022-05-01 15:41:42 -0600

    Add files via upload

---
+++ Test.c
@@ -0,0 +1,67 @@
+// Jonathan Norton
+// ITT-310: Programming for Security Professionals
+// 05/01/2022
+// Professor Jeremy Wright
+// Milestone 2
+// Test 1
+
+// Tic-Tac-Toe is intended for one player, where they will vs the computer.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+
+// These are my global variables.
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+
+// These are the function prototypes used throughout the code. They are used before the main functions to help with rebuilding
+void playerMove();
+void computerMove();
+void resetBoard();
+int checkFreeSpaces();
+void printBoard();
+void printWinner(char);
+char checkWinner();
+
+
+// Here are the main functions, which is where the main body code will go
+int main()
+{
+
+	return 0;
+}
+
+
+void playerMove()
+{
+
+}
+void computerMove()
+{
+	
+}
+void resetBoard()
+{
+
+}
+int checkFreeSpaces()
+{
+
+}
+void printBoard()
+{
+
+}
+void printWinner(char winner)
+{
+
+}
+char checkWinner()
+{
+
+}
